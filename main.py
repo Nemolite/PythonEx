b2245278a731aa4013ae5307106425781cac905a
Nemolite <g25092011@mail.ru>
2023-09-29 20:38:38 +0300

    start

---
+++ main.py
@@ -0,0 +1,46 @@
+class Example:
+    def get_data_user(self):
+        marker = True
+        while marker:
+            try:
+                a = int(input("Введите первое число = "))
+                b = int(input("Введите второе число = "))
+                ch = input("Введите операцию = ")
+                marker = False
+            except:
+                print("Вы ввели неправильно данные")
+            finally:
+                if a is None:
+                    print("Вы ввели неправильно первое число")
+                elif b is None:
+                    print("Вы ввели неправильно второе число")
+                elif ch is None:
+                    print("Вы не ввели операцию")
+                elif ch is ['+','-','/','*']:
+                    print("Вы неправильно ввели операцию")
+
+        return [a,b,ch]
+
+    def seting(self):
+        if (self.get_data_user()!=None):
+            self.a,self.b,self.ch = self.get_data_user()
+
+    def refresh(self):
+        match self.ch:
+            case '+':
+                self.res = self.a + self.b
+            case '-':
+                self.res = self.a - self.b
+            case '/':
+                self.res = self.a / self.b
+            case '*':
+                self.res = self.a * self.b
+
+    def full(self):
+        self.get_data_user()
+        self.seting()
+        self.refresh()
+        print(self.res)
+
+
+Example().full()
