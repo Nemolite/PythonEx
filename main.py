1bd46f156b4907e5e2c6ae91381b3be46f8d32f2
Nemolite <g25092011@mail.ru>
2023-10-08 17:22:54 +0300

    new

--- main.py
+++ main.py
@@ -41,6 +41,17 @@ class Exam2:
 obj2 = Exam2()
 input('Enter to stop')
 
+class Exam3:
+    def __new__(cls, *args, **kwargs):
+        print('__new__')
+        # return super().__new__(cls)
+    def __init__(self):
+        print('__init__')
+
+obj3 = Exam3()
+
+
+
 
 
 
