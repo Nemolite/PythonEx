57e73999f9626c8b273180867ec4cab787ebee02
Nemolite <g25092011@mail.ru>
2023-10-31 08:33:42 +0300

    testing

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-import sqlite3;
-from prettytable import PrettyTable
-connnectToDB = sqlite3.connect("mybasa.db")
-cursor = connnectToDB.cursor()
+import pytest
 
-# cursor.execute("""CREATE TABLE users
-#                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
-#                 First_Name TEXT,
-#                 Last_Name TEXT,
-#                 Work_exp REAL)
-#             """)
-#
-# cursor.execute("INSERT INTO users (First_Name, Last_Name, Work_exp) VALUES ('James', 'Quas',2.5)")
-# cursor.execute("INSERT INTO users (First_Name, Last_Name, Work_exp) VALUES ('Mihail', 'Vecs',5)")
-# cursor.execute("INSERT INTO users (First_Name, Last_Name, Work_exp) VALUES ('Patrik', 'Exsort',3.5)")
-# cursor.execute("INSERT INTO users (First_Name, Last_Name, Work_exp) VALUES ('Lary', 'Star',3)")
-#
-# connnectToDB.commit()
+def oper(a,b):
+    return a/b
 
-cursor.execute("SELECT * FROM users")
-newTable = PrettyTable(["ID", "First_Name", "Last_Name", "Work_exp"])
-for user in cursor.fetchall():
-    newTable.add_row([user[0], user[1], user[2], str(user[3]) + ' years'])
+def get_info_of_file():
 
-print(newTable)
+    with open("myfile.txt","r") as file:
+        list1 = file.readlines()
+
+    file.close()
+    return list(map(lambda x: x[:-1], list1))
+
